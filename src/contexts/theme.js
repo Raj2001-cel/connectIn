f357d175c0aa53e2abd02393a00ce3545fc7c0ab
Raj Mourya <rajhmourya@gmail.com>
2022-06-07 11:56:55 +0530

    added the login component

---
+++ src/contexts/theme.js
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+
+const themes = {
+  dark: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  light: {
+    backgroundColor: "white",
+    color: "black",
+  },
+};
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => {
+    localStorage.setItem("isDark", JSON.stringify(!isDark));
+    setIsDark(!isDark);
+  };
+  const theme = isDark ? themes.dark : themes.light;
+
+  useEffect(() => {
+    const isDark = localStorage.getItem("isDark") === "true";
+    setIsDark(isDark);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={[{ theme, isDark }, toggleTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
